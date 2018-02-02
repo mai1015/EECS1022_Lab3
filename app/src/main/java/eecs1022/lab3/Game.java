3b28d4896230031b0fd87faf2b5909c8170f7008
mai1015 <i@mai1015.com>
2018-02-02 13:01:38 -0500

    Basic layout

---
+++ app/src/main/java/eecs1022/lab3/Game.java
@@ -0,0 +1,14 @@
+package eecs1022.lab3;
+
+/**
+ * Created by user on 2/2/18.
+ */
+public class Game {
+    private String name;
+    private RPSGame player1;
+    private RPSGame player2;
+    
+    public Game() {
+
+    }
+}
---
+++ app/src/main/java/eecs1022/lab3/RPSGame.java
@@ -0,0 +1,10 @@
+package eecs1022.lab3;
+
+/**
+ * Created by user on 2/2/18.
+ */
+public enum RPSGame {
+    Rock,
+    Paper,
+    Scissors;
+}
