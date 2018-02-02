9a8f3b12bf4e5e0ea633238cccf5c99fc6e5478d
mai1015 <i@mai1015.com>
2018-02-01 23:03:29 -0500

    Initial commit

---
+++ app/src/main/java/eecs1022/lab3/Lab3Activity.java
@@ -0,0 +1,15 @@
+package eecs1022.lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab3Activity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab3);
+    }
+}
